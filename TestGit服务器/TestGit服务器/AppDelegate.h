7558fabd7b8d282a98c4e2b01597a88491db6c86
GaoLN <2357009363@qq.com>
2015-12-12 15:47:42 +0800

    test test

---
+++ TestGit服务器/TestGit服务器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit服务器
+//
+//  Created by 高丽楠 on 15/12/12.
+//  Copyright © 2015年 GAOLN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit服务器/TestGit服务器/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestGit服务器
+//
+//  Created by 高丽楠 on 15/12/12.
+//  Copyright © 2015年 GAOLN. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
